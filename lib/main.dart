fd1a1ebba101b0ff282e83decea9068ce950a265
fdfd <fdfdf>
2019-11-15 15:46:00 -0500

    refactor: Main file improved

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:strong_buddies_connect/themes/main_theme.dart';
 import 'package:strong_buddies_connect/user_info/user_info.dart';
+
 import 'authentication/authentication_page.dart';
 
 void main() async {
@@ -14,7 +14,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  final isLoggedIn;
+  final bool isLoggedIn;
   const MyApp(this.isLoggedIn);
 
   @override
@@ -25,12 +25,13 @@ class MyApp extends StatelessWidget {
             value: FirebaseAuth.instance.onAuthStateChanged)
       ],
       child: MaterialApp(
-          initialRoute: isLoggedIn ? '/form' : '/',
-          routes: {
-            '/': (context) => LoginPage(),
-            '/form': (context) => UserInfoPage()
-          },
-          theme: buildAppTheme()),
+        initialRoute: isLoggedIn ? '/form' : '/',
+        routes: {
+          '/': (context) => LoginPage(),
+          '/form': (context) => UserInfoPage()
+        },
+        theme: buildAppTheme(),
+      ),
     );
   }
 }
