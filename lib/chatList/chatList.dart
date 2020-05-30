8c637709aec74d6d58787343b1e7b69938673c39
samg2583@hotmail.com <samg2583@hotmail.com>
2020-05-30 00:29:57 -0500

    fix contact redirect

--- lib/chatList/chatList.dart
+++ lib/chatList/chatList.dart
@@ -238,69 +238,81 @@ class ChatListState extends State<ChatList> {
   }
 
   Widget buildContact(BuildContext context, DocumentSnapshot document) {
-    return Padding(
-      padding: EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Stack(children: <Widget>[
-            Container(
-                margin: const EdgeInsets.only(bottom: 5.0),
-                child: Material(
-                  child: document.data['photoUrl'] != null
-                      ? CachedNetworkImage(
-                          placeholder: (context, url) => Container(
-                            child: CircularProgressIndicator(
-                              strokeWidth: 1.0,
-                              valueColor:
-                                  AlwaysStoppedAnimation<Color>(themeColor),
-                            ),
-                            width: 50.0,
-                            height: 50.0,
-                            padding: EdgeInsets.all(15.0),
-                          ),
-                          imageUrl: document.data['photoUrl'],
-                          width: 50.0,
-                          height: 50.0,
-                          fit: BoxFit.cover,
-                        )
-                      : Icon(
-                          Icons.account_circle,
-                          size: 50.0,
-                          color: greyColor,
-                        ),
-                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                  clipBehavior: Clip.antiAlias,
-                )),
-            Positioned(
-                bottom: 8,
-                left: 40,
-                child: ClipOval(
-                  child: Material(
-                    color: Colors.lightGreenAccent, // button color
-                    child: InkWell(
-                      splashColor: Colors.red, // inkwell color
-                      child: SizedBox(
-                        width: 10,
-                        height: 10,
-                      ),
-                      onTap: () {},
-                    ),
-                  ),
-                ))
-          ]),
-          SizedBox(
-            width: 40, // hard coding child width
-
-            child: Text(
-              '${document.data['displayName']}',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Color(0xFF4A4A4A), fontSize: 13,fontWeight: FontWeight.w500),
+    return GestureDetector(
+        onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Chat(
+                      peerId: document.documentID,
+                      peerAvatar: document['photoUrl'],
+                      displayName: document.data['displayName'])),
             ),
-          )
-        ],
-      ),
-    );
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Stack(children: <Widget>[
+                Container(
+                    margin: const EdgeInsets.only(bottom: 5.0),
+                    child: Material(
+                      child: document.data['photoUrl'] != null
+                          ? CachedNetworkImage(
+                              placeholder: (context, url) => Container(
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 1.0,
+                                  valueColor:
+                                      AlwaysStoppedAnimation<Color>(themeColor),
+                                ),
+                                width: 50.0,
+                                height: 50.0,
+                                padding: EdgeInsets.all(15.0),
+                              ),
+                              imageUrl: document.data['photoUrl'],
+                              width: 50.0,
+                              height: 50.0,
+                              fit: BoxFit.cover,
+                            )
+                          : Icon(
+                              Icons.account_circle,
+                              size: 50.0,
+                              color: greyColor,
+                            ),
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                      clipBehavior: Clip.antiAlias,
+                    )),
+                Positioned(
+                    bottom: 8,
+                    left: 40,
+                    child: ClipOval(
+                      child: Material(
+                        color: Colors.lightGreenAccent, // button color
+                        child: InkWell(
+                          splashColor: Colors.red, // inkwell color
+                          child: SizedBox(
+                            width: 10,
+                            height: 10,
+                          ),
+                          onTap: () {},
+                        ),
+                      ),
+                    ))
+              ]),
+              SizedBox(
+                width: 40, // hard coding child width
+
+                child: Text(
+                  '${document.data['displayName']}',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Color(0xFF4A4A4A),
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 
   Widget buildItem(BuildContext context, DocumentSnapshot document) {
@@ -420,8 +432,7 @@ class ChatListState extends State<ChatList> {
                       displayName: document.data['displayName'])));
         },
         padding: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        borderSide: BorderSide(style: BorderStyle.none),
       ),
     );
   }
